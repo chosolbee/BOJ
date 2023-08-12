dded2d2d47c5ca3a84a7adfa0f90f0d869700184
chosolbee <96732525+chosolbee@users.noreply.github.com>
2023-08-13 00:37:26 +0900

    [Silver II] Title: DNA 비밀번호, Time: 328 ms, Memory: 23920 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -1,47 +1,86 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 import java.util.StringTokenizer;
 
-public class Main{
-    static int S;
-    static int P;
-    static int answer;
-    static String DNA;
-    static HashMap<Character,int[]> hm = new HashMap<Character, int[]>();
-    static char[] dna = {'A','C','G','T'};
+public class Main {
+    public static int[] checkArr = new int[4]; //비밀번호 체크 배열(A,C,G,T)
+    public static int myArr[] = new int[4]; //현재 상태 배열
+    public static int checkSecret = 0; //A,C,G,T 중 몇개가 요건에 만족하는가
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        S = Integer.parseInt(st.nextToken());
-        P = Integer.parseInt(st.nextToken());
-        DNA = br.readLine();
-        st = new StringTokenizer(br.readLine());
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
 
+        int S = Integer.parseInt(st.nextToken()); //문자열 크기
+        int P = Integer.parseInt(st.nextToken()); //부분 문자열 크기
+        int Result = 0; //정답값
+        char A[] = new char[S]; // 전체 문자열
 
-        for (char c : dna) {
-            hm.put(c, new int[] {0, Integer.parseInt(st.nextToken())});
+        A = bf.readLine().toCharArray(); // 전체 문자열 입력받아 각 char로 변환
+        st = new StringTokenizer(bf.readLine());
+        for(int i = 0; i < 4; i++){
+            checkArr[i] = Integer.parseInt(st.nextToken());
+            if(checkArr[i] == 0){ //해당 문자는 더이상 신경쓸 필요 X
+                checkSecret++; //해당 문자는 무조건 만족된다는 뜻(0개 이상 필요하므로)
+            }
         }
 
-        for (int i = 0; i < P; i++) {
-            hm.get(DNA.charAt(i))[0]++;
+        for(int i = 0; i < P; i++){ //부분 문자열 처음 받을 때 세팅
+            Add(A[i]);
         }
-        if(isFull()) answer ++;
+        if(checkSecret == 4) Result++;
 
-        for (int i = 0; i < S-P; i++) {
-            hm.get(DNA.charAt(i))[0] -= 1;
-            hm.get(DNA.charAt(i + P))[0] += 1;
-            if(isFull()) answer ++;
+        // 슬라이딩 윈도우
+        for(int i = P; i < S; i++){ // i는 우측 포인터
+            // i - j == P가 되어야 하므로
+            int j = i - P; // j는 좌측 포인터
+            Add(A[i]);
+            Remove(A[j]);
+            if(checkSecret == 4) Result++;
         }
-        System.out.println(answer);
-
+        System.out.println(Result);
+        bf.close();
     }
-    public static boolean isFull() {
-        for (char c : dna) {
-            if (hm.get(c)[0] < hm.get(c)[1]) return false;
-        }
-        return true;
 
+    private static void Add(char c) { //신규로 들어오는 문자열 개수 업데이트
+        switch (c){
+            case 'A' :
+                myArr[0]++; //A에 해당하는 myArr 0번째 index개수 증가
+                if(myArr[0] == checkArr[0]) checkSecret++; //checkArr와 동일할 시, checkSecret 증가
+                break;
+            case 'C' :
+                myArr[1]++;
+                if(myArr[1] == checkArr[1]) checkSecret++;
+                break;
+            case 'G' :
+                myArr[2]++;
+                if(myArr[2] == checkArr[2]) checkSecret++;
+                break;
+            case 'T' :
+                myArr[3]++;
+                if(myArr[3] == checkArr[3]) checkSecret++;
+                break;
+        }
+    }
+    private static void Remove(char c) { //빠지는 문자열 개수 업데이트
+        switch (c){
+            case 'A' :
+                if(myArr[0] == checkArr[0]) checkSecret--;
+                myArr[0]--;
+                break;
+            case 'C' :
+                if(myArr[1] == checkArr[1]) checkSecret--;
+                myArr[1]--;
+                break;
+            case 'G' :
+                if(myArr[2] == checkArr[2]) checkSecret--;
+                myArr[2]--;
+                break;
+            case 'T' :
+                if(myArr[3] == checkArr[3]) checkSecret--;
+                myArr[3]--;
+                break;
+        }
     }
 }
